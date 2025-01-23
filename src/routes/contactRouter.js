2f8edbf79c0ce670676f63747e909943dc80b2c4
Zev Spitz <shivisi@hotmail.com>
2025-01-23 14:52:43 +0200

    Endpoints: Add/remove contact info

--- src/routes/contactRouter.js
+++ src/routes/contactRouter.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import Contact from '../db/models/contact.js';
-import { contactValidationSchema } from '../validations/contact.js';
+import { contactValidationSchema, contactInfoValidationSchema } from '../validations/contact.js';
 
 const contactRouter = Router();
 
@@ -40,5 +40,39 @@ contactRouter.delete('/:id', async (req, res) => {
     res.status(200).send(req.contact);
 });
 
+contactRouter.post('/:id/contactinfos', async (req, res) => {
+    try {
+        await contactInfoValidationSchema.validateAsync(req.body);
+    } catch (error) {
+        res.status(400).send(error);
+        return;
+    }
+    req.contact.contactInfos.push(req.body);
+    req.contact.save();
+    res.status(200).send(req.contact);
+});
+
+contactRouter.delete('/:id/contactinfos', async (req, res) => {
+    try {
+        await contactInfoValidationSchema.validateAsync(req.body);
+    } catch (error) {
+        res.status(400).send(error);
+        return;
+    }
+
+    const index = req.contact.contactInfos.findIndex(ci => {
+        return (
+            ci.infoType === req.body.infoType &&
+            ci.value === req.body.value
+        );
+    });
+    if (index === -1) {
+        res.status(404).send('Contact info not found');
+        return;
+    }
+    req.contact.contactInfos.splice(index, 1);
+    req.contact.save();
+    res.status(200).send(req.contact);
+});
 
 export { contactRouter };
--- src/validations/contact.js
+++ src/validations/contact.js
@@ -1,18 +1,37 @@
 import Joi from 'joi';
 
-const contactValidationSchema = Joi.object({
-    firstName: Joi.string()
-        .alphanum()
-        .min(2),
-    lastName: Joi.string()
-        .alphanum()
-        .min(2)
-        .required(),
-    contactInfos: Joi.array().items(
-        Joi.object({
-            infoType: Joi.string().pattern(/^[a-zA-Z]*$/).min(5),
-            value: Joi.string()
-        }))
-});
+// phone, email. website
 
-export { contactValidationSchema };
+const contactInfoValidationSchema =
+    Joi.alternatives().try(
+        Joi.object({
+            infoType: Joi.string().valid('email'),
+            value: Joi.string().email()
+        }),
+        Joi.object({
+            infoType: Joi.string().valid('website'),
+            value: Joi.string().uri()
+        }),
+        Joi.object({
+            infoType: Joi.string().valid('phone'),
+            value: Joi.string().pattern(/^[\D-]*$/)
+        }),
+        Joi.object({
+            infoType: Joi.string().pattern(/^[a-zA-Z0-9_]*$/).min(5),
+            value: Joi.string()
+        })
+    );
+
+const contactValidationSchema =
+    Joi.object({
+        firstName: Joi.string()
+            .alphanum()
+            .min(2),
+        lastName: Joi.string()
+            .alphanum()
+            .min(2)
+            .required(),
+        contactInfos: Joi.array().items(contactInfoValidationSchema)
+    });
+
+export { contactValidationSchema, contactInfoValidationSchema };
